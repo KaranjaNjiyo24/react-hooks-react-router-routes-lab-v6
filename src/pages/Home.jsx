09538ec05cb0bbcc0ebc93775c5085be066b3ea5
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-11-15 01:35:49 +0300

    Adds routing and Errorpage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+  }, [])
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
+      <NavBar />
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        <section>
+          {movies.map((movie) => (
+            <MovieCard 
+              key={movie.id}
+              title={movie.title}
+              id={movie.id}
+            />
+          ))}
+        </section>
       </main>
     </>
   );
